128c7a1ea6bf2f3299fbaa81958a35ebed20c1e0
Jean Mermoz Effi <jeanmermozeffi@gmail.com>
2023-08-17 04:34:12 +0100

    personnalisation de la page des pages d'erreurs

---
+++ src/PS/SiteBundle/Controller/ExceptionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PS\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ExceptionController extends Controller
+{
+    public function showExceptionAction(\Throwable $exception)
+    {
+        if ($exception instanceof HttpExceptionInterface) {
+            $statusCode = $exception->getStatusCode();
+        } else {
+            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        }
+
+        return $this->render('@Twig/Exception/error' . $statusCode . '.html.twig', [
+            'exception' => $exception,
+            'status_code' => $statusCode,
+        ]);
+    }
+}
