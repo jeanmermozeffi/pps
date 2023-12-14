41a70bb02c410a4c404d10c0dd5af7f3cb6218d2
Jean Mermoz Effi <jeanmermozeffi@gmail.com>
2023-12-14 01:05:46 +0100

    Correction de l'affichage

--- src/PS/GestionBundle/Service/BadgeEditionPatient.php
+++ src/PS/GestionBundle/Service/BadgeEditionPatient.php
@@ -106,7 +106,6 @@ class BadgeEditionPatient
 
         $template = '/patient/badge-multiple.html.twig';
         // $template = '/patient/badge.html.twig';
-        $template = '/patient/test-badge.html.twig';
         // Générez le contenu HTML pour chaque patient
         foreach ($selectedPatients as $index => $patient) {
             // On stocke la vue à convertir en PDF, en n'oubliant pas les paramètres twig si la vue comporte des données dynamiques
